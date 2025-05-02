e8e3a0fd207ef81dec997f41420bae39f41594af
fmzu <149039422+fmzu@users.noreply.github.com>
2025-05-03 00:40:36 +0900

    update: タブのアイコンを修正する

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,46 +1,38 @@
 import { Tabs } from "expo-router"
-import { Platform } from "react-native"
 
-import { HapticTab } from "@/components/HapticTab"
-import { IconSymbol } from "@/components/ui/IconSymbol"
-import TabBarBackground from "@/components/ui/TabBarBackground"
-import { Colors } from "@/constants/Colors"
-import { useColorScheme } from "@/hooks/useColorScheme"
+import Ionicons from "@expo/vector-icons/Ionicons"
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme()
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: "absolute",
-          },
-          default: {},
-        }),
+        tabBarActiveTintColor: "#ffd33d",
       }}
+      height={180}
     >
       <Tabs.Screen
         name="index"
         options={{
           title: "メッセージ",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="house.fill" color={color} />
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "mail-sharp" : "mail-outline"}
+              color={color}
+              size={32}
+            />
           ),
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: "写真",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="paperplane.fill" color={color} />
+          title: "アルバム",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "camera" : "camera-outline"}
+              color={color}
+              size={32}
+            />
           ),
         }}
       />
